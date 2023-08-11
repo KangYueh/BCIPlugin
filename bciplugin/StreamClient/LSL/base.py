807f374f14bbf502e77085ab358765c4b4a6bbf3
hehehe <einhep@163.com>
2023-08-11 20:01:59 +0800

    go through the lsl code (part1)

--- bciplugin/StreamClient/LSL/base.py
+++ bciplugin/StreamClient/LSL/base.py
@@ -1,29 +1,41 @@
 import time
 
 import mne
-from pylsl import StreamInlet,resolve_stream
+from pylsl import StreamInlet, resolve_stream
 import numpy as np
-from threading import Thread,Timer
+from threading import Thread, Timer
 import matplotlib.pyplot as plt
 
-from StreamClient.buffer import EEGBuffer
-from StreamClient.vis import StreamWindow
-from StreamClient.record import StreamRecorder
+from bciplugin.StreamClient.buffer import EEGBuffer
+from bciplugin.StreamClient.vis import StreamWindow
+from bciplugin.StreamClient.record import StreamRecorder
 
 
+def get_array_from_container(container):
+    """ concatenate them into a single numpy array (2d) and return it
 
-def getArrayFromContainer(container):
+    Parameters
+    ----------
+    container: list
+        a list of dict. The structure of this dict is {'eeg': data, 'receivedSamples': sample.shape[1]}
+
+    Returns
+    -------
+        eeg_array: numpy.ndarray
+            2-dim eeg data
+    """
     data = [c['eeg'] for c in container]
-    eegArray = np.concatenate(data,axis=1)
-    return eegArray
+    eeg_array = np.concatenate(data, axis=1)
+    return eeg_array
+
 
 class LSLClient(StreamWindow, StreamRecorder):
-    def __init__(self, ch_names, BufferPeriod=5, verbose=False, LSLType='EEG'):
-        self.BufferPeriod = BufferPeriod
+    def __init__(self, channel_names, buffer_period=5, verbose=False, lsl_type='EEG'):
+        self.BufferPeriod = buffer_period
         self.verbose = verbose
         self.inlet = None
-        self.LSLType = LSLType
-        self.ch_names = ch_names
+        self.LSLType = lsl_type
+        self.ch_names = channel_names
 
         self.isRecording = False
         self.isConnected = False
@@ -31,15 +43,14 @@ class LSLClient(StreamWindow, StreamRecorder):
         self.Buffer = None
         self.buffer = None
         self.infoList = {
-            'ch_names':[],
-            'mne_info':None,
-            'sfreq':None
+            'ch_names': [],
+            'mne_info': None,
+            'sfreq': None
         }
 
         self.raw_container = []
         self.curRawRecording = None
         self.raw_filename = None
-
 
     def startStreaming(self):
         if not self.isConnected:
@@ -59,13 +70,13 @@ class LSLClient(StreamWindow, StreamRecorder):
         self.inlet = inlet
 
     def setInfoList(self, ch_names, ch_types):
-        self.infoList['mne_info'] = mne.create_info(ch_names=ch_names,sfreq=self.infoList['sfreq'],ch_types='eeg')
+        self.infoList['mne_info'] = mne.create_info(ch_names=ch_names, sfreq=self.infoList['sfreq'], ch_types='eeg')
         self.infoList['mne_info'].set_montage('standard_1020')
         self.infoList['ch_names'] = ch_names
         self.infoList['ch_types'] = ch_types
 
     def getInfoList(self):
-        #自动获取InfoList
+        # 自动获取InfoList
         pass
 
     def parseInfoList(self):
@@ -83,17 +94,17 @@ class LSLClient(StreamWindow, StreamRecorder):
 
     def index2pos(self, idx, dim=2):
         info = self.infoList['mne_info']
-        if dim==2:
-            return np.array([info['chs'][idx]['loc'][0],info['chs'][idx]['loc'][1]])
-        if dim==3:
-            return np.array([info['chs'][idx]['loc'][0],info['chs'][idx]['loc'][1],info['chs'][idx]['loc'][2]])
+        if dim == 2:
+            return np.array([info['chs'][idx]['loc'][0], info['chs'][idx]['loc'][1]])
+        if dim == 3:
+            return np.array([info['chs'][idx]['loc'][0], info['chs'][idx]['loc'][1], info['chs'][idx]['loc'][2]])
 
     def receiving_loop(self):
         Timer(interval=1, function=self.receiving_loop).start()
 
         dataList = {}
         samples, timestamps = self.inlet.pull_chunk(max_samples=int(self.infoList['sfreq']))
-        if len(samples)>0:
+        if len(samples) > 0:
             samples = np.transpose(np.array(samples))
             self.samples = samples
             dataList['eeg'] = samples
@@ -101,7 +112,6 @@ class LSLClient(StreamWindow, StreamRecorder):
             self.Buffer.putData(dataList)
             if self.isRecording:
                 self.raw_container.append(dataList)
-
 
     def initTopoDisplay(self, channels):
         self.visTopoFigure = plt.figure()
@@ -117,7 +127,6 @@ class LSLClient(StreamWindow, StreamRecorder):
         for i, chan in enumerate(self.waveChannels):
             self.waveAxes.append(self.visWaveFigure.add_subplot(int(str(len(channels)) + '1' + str(i + 1))))
 
-
     def startWaveDisplay(self):
         Timer(interval=1, function=self.startWaveDisplay).start()
 
@@ -128,7 +137,6 @@ class LSLClient(StreamWindow, StreamRecorder):
             self.waveAxes[i].set_xticklabels('')
             self.waveAxes[i].plot(data[i])
             self.waveAxes[i].figure.canvas.draw()
-
 
     def startTopoDisplay(self):
         Timer(interval=3, function=self.startTopoDisplay).start()
@@ -146,12 +154,10 @@ class LSLClient(StreamWindow, StreamRecorder):
 
         self.topoAxes.figure.canvas.draw()
 
-
     def initRecording(self, filename):
         self.raw_filename = filename
         self.raw_container = []
         self.isRecording = False
-
 
     def startRecording(self, **kwargs):
         self.isRecording = True
@@ -161,14 +167,15 @@ class LSLClient(StreamWindow, StreamRecorder):
         ch_names, sfreq, ch_types = self.parseInfoList()
         info = mne.create_info(ch_names=ch_names, sfreq=sfreq, ch_types=ch_types)
         info.set_montage('standard_1020')
-        data = getArrayFromContainer(self.raw_container)
+        data = get_array_from_container(self.raw_container)
         # convert to uv
         if np.abs(data.mean()) > 1e-3:
             data *= 1e-6
         self.curRawRecording = mne.io.RawArray(data=data, info=info)
 
+
 ch_names = ['T7', 'T8', 'C3', 'C4', 'F3', 'F4', 'P3', 'P4']
-lc = LSLClient(ch_names=ch_names)
+lc = LSLClient(channel_names=ch_names)
 lc.startStreaming()
-lc.initWaveDisplay(channels=[0,1,2])
+lc.initWaveDisplay(channels=[0, 1, 2])
 lc.startWaveDisplay()
